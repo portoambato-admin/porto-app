63d79c343b9c1c29e7306c6a02bd9ae932fbaa43
RogersBury <rogersbury@gmail.com>
2025-08-12 13:06:09 -0500

    Agregar build web para Render

--- app_porto/build/web/flutter_service_worker.js
+++ app_porto/build/web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1706a9aa897f7f6f721400a3e68816ed
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f1ef4950c20bc6342e12f8706645df83",
+"flutter_bootstrap.js": "8be3fdf2858d7bf8c864884bc4a4fe73",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
